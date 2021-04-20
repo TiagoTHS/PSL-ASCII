6152a54ccf6f796907b5a0d2e459ae91c9192f6b
Tiago Souza <61852650+TiagoTHS@users.noreply.github.com>
2021-04-20 15:46:29 -0300

    First

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function edu(obj)
+{
+    obj.innerHTML="> Estudo Ciência da computação na UFU, 7periodo<br>"+
+                  "> Certificado de Ingles avancado<br>"+
+                  "> Certificado de maratona de programação<br>";
+}
+        
+function hab(obj)
+{
+    obj.innerHTML="> C <br>"+
+                  "> Java: conhecimento médio, já utilizei em trabalhos para faculdade<br>"+
+                  "> JavaScript: conhecimento avançado, utilizei em projetos com nodejs, express<br>"+
+                  "> Banco de dados";
+}
+
+function soft(obj)
+{
+    obj.innerHTML="> Trabalho em grupo<br>"+
+                  "> Comunicativo";
+}
